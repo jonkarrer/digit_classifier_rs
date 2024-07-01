9b2d804d327b9bb9078069d8249d632603a918f2
jkarrer <jkarrer@protect247.com>
2024-07-01 14:05:55 -0400

    feat: set up a stochastic gradient example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use candle_core::Tensor;
-use candle_nn::loss::mse;
+use candle_core::{DType, Device, Tensor, TensorId, Var};
+use candle_nn::{loss::mse, Optimizer, VarBuilder, SGD};
 use std::{fs, path::Path};
 
 fn dir_walk(dir_path: &str) -> anyhow::Result<Vec<String>> {
@@ -91,51 +91,112 @@ fn accuracy_measure(matching_set_eval: &Tensor, mismatching_set_eval: &Tensor) -
 }
 
 fn main() -> anyhow::Result<()> {
-    // Get the images from the directories and convert them to tensors
-    let threes_tensor_list = transform_dir_into_tensors("dataset/training/3");
-    let sevens_tensor_list = transform_dir_into_tensors("dataset/training/7");
+    // // Get the images from the directories and convert them to tensors
+    // let threes_tensor_list = transform_dir_into_tensors("dataset/training/3");
+    // let sevens_tensor_list = transform_dir_into_tensors("dataset/training/7");
 
-    // Stack tensors on one axis
-    let stacked_three_tensor = stack_tensors_on_axis(&threes_tensor_list, 0);
-    let stacked_seven_tensor = stack_tensors_on_axis(&sevens_tensor_list, 0);
+    // // Stack tensors on one axis
+    // let stacked_three_tensor = stack_tensors_on_axis(&threes_tensor_list, 0);
+    // let stacked_seven_tensor = stack_tensors_on_axis(&sevens_tensor_list, 0);
 
-    // Calculate the ideal shape of the digit by averaging the stack of all the digits on the first axis
-    let ideal_three = average_tensor_on_axis(&stacked_three_tensor, 0);
-    let ideal_seven = average_tensor_on_axis(&stacked_seven_tensor, 0);
+    // // Calculate the ideal shape of the digit by averaging the stack of all the digits on the first axis
+    // let ideal_three = average_tensor_on_axis(&stacked_three_tensor, 0);
+    // let ideal_seven = average_tensor_on_axis(&stacked_seven_tensor, 0);
 
-    // Get the first sample from each directory as a tensor
-    let sample_three = &stacked_three_tensor.get_on_dim(0, 10).unwrap();
-    let sample_seven = &stacked_seven_tensor.get_on_dim(0, 3).unwrap();
+    // // Get the first sample from each directory as a tensor
+    // let sample_three = &stacked_three_tensor.get_on_dim(0, 10).unwrap();
+    // let sample_seven = &stacked_seven_tensor.get_on_dim(0, 3).unwrap();
 
-    // Calculate the loss between the ideal and the sample
-    let three_mse_loss = mse(&sample_three, &ideal_three).unwrap();
-    let seven_mse_loss = mse(&sample_seven, &ideal_seven).unwrap();
-    dbg!(three_mse_loss, seven_mse_loss);
+    // // Calculate the loss between the ideal and the sample
+    // let three_mse_loss = mse(&sample_three, &ideal_three).unwrap();
+    // let seven_mse_loss = mse(&sample_seven, &ideal_seven).unwrap();
+    // dbg!(three_mse_loss, seven_mse_loss);
 
-    let three_mnist_distance = mnist_distance(&sample_three, &ideal_three);
-    let seven_mnist_distance = mnist_distance(&sample_three, &ideal_seven);
-    dbg!(three_mnist_distance, seven_mnist_distance);
+    // let three_mnist_distance = mnist_distance(&sample_three, &ideal_three);
+    // let seven_mnist_distance = mnist_distance(&sample_three, &ideal_seven);
+    // dbg!(three_mnist_distance, seven_mnist_distance);
 
-    // Prepare validation set
-    let valid_threes_tensor_list = transform_dir_into_tensors("dataset/validation/groups/3");
-    let valid_sevens_tensor_list = transform_dir_into_tensors("dataset/validation/groups/7");
-    let valid_stacked_three_tensor = stack_tensors_on_axis(&valid_threes_tensor_list, 0);
-    let valid_stacked_seven_tensor = stack_tensors_on_axis(&valid_sevens_tensor_list, 0);
+    // // Prepare validation set
+    // let valid_threes_tensor_list = transform_dir_into_tensors("dataset/validation/groups/3");
+    // let valid_sevens_tensor_list = transform_dir_into_tensors("dataset/validation/groups/7");
+    // let valid_stacked_three_tensor = stack_tensors_on_axis(&valid_threes_tensor_list, 0);
+    // let valid_stacked_seven_tensor = stack_tensors_on_axis(&valid_sevens_tensor_list, 0);
 
-    // Calculate the loss on the validation set
-    let valid_three_mnist_distance = mnist_distance(&valid_stacked_three_tensor, &ideal_three);
-    let valid_seven_mnist_distance = mnist_distance(&valid_stacked_seven_tensor, &ideal_seven);
-    dbg!(valid_three_mnist_distance, valid_seven_mnist_distance);
+    // // Calculate the loss on the validation set
+    // let valid_three_mnist_distance = mnist_distance(&valid_stacked_three_tensor, &ideal_three);
+    // let valid_seven_mnist_distance = mnist_distance(&valid_stacked_seven_tensor, &ideal_seven);
+    // dbg!(valid_three_mnist_distance, valid_seven_mnist_distance);
 
-    // Check if the sample is a three
-    let test_a_three = is_a_three(sample_three, &ideal_three, &ideal_seven);
-    dbg!(test_a_three.to_scalar::<f32>().unwrap() == 1.0);
+    // // Check if the sample is a three
+    // let test_a_three = is_a_three(sample_three, &ideal_three, &ideal_seven);
+    // dbg!(test_a_three.to_scalar::<f32>().unwrap() == 1.0);
 
-    // Check if the validation set is a three
-    let three_training_run = is_a_three(&valid_stacked_three_tensor, &ideal_three, &ideal_seven);
-    let seven_training_run = is_a_three(&valid_stacked_seven_tensor, &ideal_three, &ideal_seven);
+    // // Check if the validation set is a three
+    // let three_training_run = is_a_three(&valid_stacked_three_tensor, &ideal_three, &ideal_seven);
+    // let seven_training_run = is_a_three(&valid_stacked_seven_tensor, &ideal_three, &ideal_seven);
 
-    dbg!(accuracy_measure(&three_training_run, &seven_training_run));
+    // dbg!(accuracy_measure(&three_training_run, &seven_training_run));
+
+    // Simple gradient descent example
+    // let xt = Tensor::from_vec(vec![3.0, 4.0, 6.0], 3, &candle_core::Device::Cpu)?;
+    // let vars = Var::from_tensor(&xt)?;
+    // dbg!(vars.powf(2.0).unwrap().backward()?);
+
+    // SGD Example
+    let time = Tensor::arange(1.0, 20.0, &Device::Cpu)?;
+    let speed = &Tensor::rand(0.0, 70.0, 19, &Device::Cpu)?;
+
+    // 1. Initialize parameters
+    let params = Var::from_tensor(&Tensor::randn(35.0, 10.0, 3, &Device::Cpu)?)?;
+    dbg!(&params);
+
+    // 2. Calculate predictions
+    let predictions = quad(&time, &params)?;
+
+    // 3. Calculate loss
+    let loss = mse(&predictions, &speed)?;
+
+    // 4. Calculate gradients
+    let mut optimizer = SGD::new(vec![params.clone()], 1e-5)?;
+    optimizer.backward_step(&loss)?;
+
+    // 5. Step the weights
+    // 6. Repeat
+    for _ in 0..10 {
+        let step_result = apply_step(&params, &time, &speed)?;
+        dbg!(&step_result);
+    }
+
+    // 7. Stop
+    dbg!(&params);
 
     Ok(())
+}
+
+fn apply_step(params: &Var, time: &Tensor, speed: &Tensor) -> anyhow::Result<Tensor> {
+    // 2. Calculate predictions
+    let predictions = quad(&time, &params)?;
+
+    // 3. Calculate loss
+    let loss = mse(&predictions, &speed)?;
+
+    // 4. Calculate gradients
+    let mut optimizer = SGD::new(vec![params.clone()], 1e-5)?;
+    optimizer.backward_step(&loss)?;
+
+    Ok(loss)
+}
+
+fn quad(time: &Tensor, parameters: &Var) -> anyhow::Result<Tensor> {
+    let a = parameters.get(0)?;
+    let b = parameters.get(1)?;
+    let c = parameters.get(2)?;
+
+    let t_squared = time.powf(2.0)?;
+    let term_1 = a.broadcast_mul(&t_squared)?;
+    let term_2 = b.broadcast_mul(&time)?;
+
+    let res = term_1.add(&term_2)?.broadcast_add(&c)?;
+
+    Ok(res)
 }
