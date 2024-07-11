ff0b6e957cfca1bdcaa0f676e789f351e4068704
jkarrer <jkarrer@protect247.com>
2024-07-11 13:50:59 -0400

    add: create a basic loss

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,99 @@
 mod comparing_raw_loss;
-mod stochastic_gradient_decent;
+mod simple_sgd_model;
 mod tensor_tools;
 
 use std::ops::{Add, Mul};
 
-use candle_core::{DType, Device, Tensor, Var};
-use candle_nn::{linear, Linear, VarBuilder, VarMap};
+use candle_core::{DType, Device, Result, Tensor, Var};
+use candle_nn::{linear, ops::sigmoid, VarBuilder, VarMap};
 use tensor_tools::{stack_tensors_on_axis, transform_dir_into_tensors};
 
 struct Dataset {
-    training_examples: Tensor,
-    training_results: Tensor,
+    training_inputs: Tensor,
+    training_labels: Tensor,
 }
 
-struct Model {
-    lnn_one: Linear,
-    lnn_two: Linear,
-    lnn_three: Linear,
+struct Linear {
+    weights: Tensor,
+    bias: Tensor,
+}
+
+impl Linear {
+    fn forward(&self, inputs: &Tensor) -> Result<Tensor> {
+        let x = inputs.matmul(&self.weights)?;
+        x.broadcast_add(&self.bias)
+    }
 }
 
 fn main() -> anyhow::Result<()> {
     let device = Device::Cpu;
 
+    // ** 0. Prepare dataset
     // Get the images from the directories and convert them to tensors
     let threes_tensor_list = transform_dir_into_tensors("dataset/training/3");
     let sevens_tensor_list = transform_dir_into_tensors("dataset/training/7");
-    dbg!(threes_tensor_list.len(), sevens_tensor_list.len());
-    dbg!(threes_tensor_list[0].dims(), sevens_tensor_list[0].dims());
-
-    // Stack tensors on one axis
     let stacked_three_tensor = stack_tensors_on_axis(&threes_tensor_list, 0);
     let stacked_seven_tensor = stack_tensors_on_axis(&sevens_tensor_list, 0);
-    dbg!(stacked_three_tensor.shape(), stacked_seven_tensor.shape());
 
-    // // Concat tensors for x axis
+    // Concat tensors for x axis and reshape to 2-dimensional tensor
+    // Rank 2:[[pixel; 784]; 8752]
     let training_set_x = Tensor::cat(&[&stacked_three_tensor, &stacked_seven_tensor], 0)?;
-    dbg!(&training_set_x.shape());
+    let training_inputs = training_set_x.reshape(((), 28 * 28))?;
 
-    // // Reshape n-dimensional tensor to 2-dimensional tensor
-    let training_set_x = training_set_x.reshape(((), 28 * 28))?;
-    dbg!(&training_set_x.shape());
+    // Create target tensor using 1 for 3 and 0 for 7
+    // Rank 2: [[label]; 8752]
+    let mut labels: Vec<f32> = vec![1f32; threes_tensor_list.len()];
+    labels.extend(vec![0f32; sevens_tensor_list.len()]);
+    let dim_length = labels.len();
+    let training_labels = Tensor::from_vec(labels, (dim_length, 1), &device)?;
 
-    // // Create target tensor using 1 for 3 and 0 for 7
-    // let mut target_threes: Vec<u8> = vec![1; threes_tensor_list.len()];
-    // let target_sevens: Vec<u8> = vec![0; sevens_tensor_list.len()];
-    // target_threes.extend(target_sevens);
-    // let first_dimension = target_threes.len();
+    let dataset = Dataset {
+        training_inputs,
+        training_labels,
+    };
 
-    // let training_set_y = Tensor::from_vec(target_threes, &[first_dimension, 1], &device)?;
-    // dbg!(training_set_y.shape());
+    // ** 1. Initialize parameters (weights and biases)
+    // Weights must be the same shape as one indices tensor of X. In this case X[0] is a 784 item vector, [[0-255; 784]].
+    // Rank 2: [[random_num]; 784]
+    let weights = Tensor::randn(0f32, 1.0, (28 * 28, 1), &device)?;
 
-    // // Prepare input tensors
-    // let dataset = Dataset {
-    //     training_examples: training_set_x,
-    //     training_results: training_set_y,
-    // };
-
-    // 1. Initialize parameters (weights and biases)
-    // Weights must be the same shape as one index tensor of X. In this case 1 index of X is a 28x28 image tensor. The whole X axis is thousands of images, but we just want to match the size of one.
-    let weights = Var::from_tensor(&Tensor::randn(
-        1.0 as f32,
-        1.0 as f32,
-        (28 * 28, 1),
-        &device,
-    )?)?;
     // Bias can just be a random scalar
-    let biases = Var::from_tensor(&Tensor::randn(0.0 as f32, 1.0 as f32, (1, 1), &device)?)?;
-    dbg!(&weights.shape(), &biases.shape());
+    // [[random_num]]
+    let biases = Tensor::randn(0f32, 1.0, 1, &device)?;
 
-    // 2. Calculate predictions. The root function here is the linear function, y = wx + b, where w is the weights tensor, x is the input tensor, and b is the bias tensor.
-    // The wx multiplication is a matrix multiplication. It's a 28x28 matrix times 28x28 matrix.
-    let prediction = linear_layer(&training_set_x, &weights, &biases)?;
-    dbg!(&prediction);
+    let model = Linear {
+        weights: weights,
+        bias: biases,
+    };
+
+    // ** 2. Calculate predictions.
+    //The root function here is the linear function, y = wx + b,
+    // Where w is the weights tensor, x is the input tensor, and b is the bias tensor.
+    let predict_just_one = dataset
+        .training_inputs
+        .get(0)? // Get first row [0-255; 784]
+        .mul(&model.weights.squeeze(1)?)? // Multiply each item by each weight, [0-255; 784] * [random_num; 784]
+        .sum_all() // Sum all the items in the new vector [products; 784] = [scalar]
+        .add(model.bias.get(0)?)?; // Add the bias to the scalar. scalar + bias = [scalar]
+
+    // We can use matrix multiplication to do the same thing, but on each row of the rank 2 tensors.
+    // Weights and inputs are both rank 2, so [[weights]; 784] x [[pixels; 784]; 8752] = [[predictions]; 8752]
+    let predictions = model.forward(&dataset.training_inputs)?;
+
+    // 3. Calculate loss
+    let t = Tensor::from_vec(vec![1f32, 0f32, 1f32], &[3], &device)?;
+    let p = Tensor::from_vec(vec![0.9f32, 0.4f32, 0.2f32], &[3], &device)?;
+    let loss = loss(&p, &t, &device)?;
+    dbg!(&loss);
+
     Ok(())
 }
 
-fn linear_layer(inputs: &Tensor, weights: &Var, biases: &Var) -> anyhow::Result<Tensor> {
-    let slope_mul_inputs = inputs.matmul(&weights.as_tensor())?;
-    let add_bias = slope_mul_inputs.broadcast_add(biases.as_tensor())?;
-    Ok(add_bias)
+fn loss(prediction: &Tensor, target: &Tensor, device: &Device) -> Result<Tensor> {
+    // Force into values to be between 0 and 1
+    // let prediction = sigmoid(prediction)?;
+
+    target
+        .eq(1f32)?
+        .where_cond(&Tensor::ones_like(&target)?.sub(&prediction)?, &prediction)
 }
